54fc21b7d3904e9533e592a6d87981b0fca54d76
Nemesiscod3 <g1psyaveng3r@gmail.com>
2022-11-09 22:34:51 -0500

    rock,paper,scissors 1.0

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function getcomputerchoice(){
 
-
+    choices = [ "Rock, Paper, Scissors"];
+    var ai = choices[Math.floor(Math.random() * choices.length)]
+    return ai;
 }
-
-
 
 function PlayRound(){
 
