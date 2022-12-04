499045d2f25c2622b29e22702fa23aa61a61e733
Nemesiscod3 <g1psyaveng3r@gmail.com>
2022-12-03 23:43:17 -0500

    rock-paper-scissors 0.2

--- script.js
+++ script.js
@@ -9,8 +9,36 @@ function computerplay() {
 computerplay();
 
 
-function PlayRound(playersection, computerselection){
+function playRound(playerSelection, computerSelection) {
+ 
+window.prompt("welcome to a game of rack paper scissors play a round of rock paper sissors")
+
+
+if(playerSelection === computerSelection)
+{
+  return  "it's a tie";
+} if (playerSelection = "rock", computerSelection = "paper") {
+
+  return "you lose"
+  
+} else if(playerSelection = "paper", computerSelection = "scissors"){
+
+  return "you lose"
+
+
+}else {
+
+  return "player won"
+}
+
+
+
+}
+
+playRound();
+   
+const computerSelection = computerplay();
+
+  
 
     
-    
-}
