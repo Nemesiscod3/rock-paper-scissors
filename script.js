723885ebfe59b1a08a6a2ffea65c6bcb275cd921
Nemesiscod3 <g1psyaveng3r@gmail.com>
2023-02-07 16:56:19 -0500

    rock-paper-scissors javascript 1.0

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-
-
-
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"]
   const ai = choices[Math.floor(Math.random() * choices.length)]
@@ -19,33 +16,35 @@ playerSelection = prompt('welcome to rock paper scissors');
 
 if (playerSelection.toLowerCase() === computerSelection) {
 
-  alert('its a tie');
+  roundResult = ('its a tie');
   
 }else if(playerSelection == "rock" && computerSelection == "paper") {
 
-  alert('you lose')
+  roundResult = ('you lose this round')
 
 }else if(playerSelection == "paper" && computerSelection == "scissors") {
 
-  alert('you lose')
+  roundResult = ('you lose this round')
 
 }else if (playerSelection == "scissors" && computerSelection == "rock"){
 
-  alert('you lose')
+  roundResult = ('you lose this round')
 }else if (playerSelection == "rock" && computerSelection == "scissors"){
 
-  alert('you win')
+  roundResult = ('you win this round')
 
 }else if (playerSelection == "paper" && computerSelection == "rock"){
 
-  alert('you win')
+  roundResult = ('you win this round')
 
 }else if(playerSelection == "scissors" && computerSelection == "paper"){
 
-  alert('you win')
+  roundResult = ('you win this round')
 
 }
 
+return roundResult
+ 
 }
 
 playRound();
@@ -56,21 +55,15 @@ let playerscore = 0;
 let computerscore = 0;
 
 
-function scoreboard() {
-  playRound(playerSelection,computerSelection);
-  if (playerscore>computerscore){
-  console.log("Player Won");
-   }
-  if (computerscore>playerscore) {
-  console.log("Computer Win");
-   }
- }
-
- //to loop the game 5 times//
- function game() {
-  for (let i=0; i<4; i++) {
-  scoreboard()
-    }
+function game() {
+  for (let i = 0; i < 4; i++) {
+       computerSelection = getComputerChoice();
+      playRound(playerSelection, computerSelection);
+      console.log(roundResult);
   }
+  
+}
 
-  game();
+console.log(roundResult)
+
+game();
