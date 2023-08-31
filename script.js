7edc1e321627b43fe77e72e1c29284ca45ff8549
Nemesiscod3 <g1psyaveng3r@gmail.com>
2023-08-31 00:12:58 -0400

    rock,paper,scissors ui

--- script.js
+++ script.js
@@ -1,76 +1,95 @@
-let playerSelection ;
-computerSelection = getComputerChoice()
 let playertext = document.querySelector('#plays-selection')
 let computerText = document.querySelector('#computer-selection')
 let resultText = document.querySelector('#result')
+const buttons = document.querySelectorAll('button')
+const computercounter = document.querySelector('#computer-count')
+const playercounter = document.querySelector('#player-count')
+let result 
+let playerscore = 0;
+let computerscore = 0;
+
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"]
-  const ai = choices[Math.floor(Math.random() * choices.length)]
+  const ai = Math.floor(Math.random() * choices.length);
+  return choices[ai]
 
-  
-  return ai
 }
 
-getComputerChoice();
 
 
-const buttons = document.querySelectorAll('button')
+function playround (playerSelection,computerSelection) {
 
-buttons.forEach((button) =>{
+  if (!computerSelection) {
+    computerSelection = getComputerChoice();
+  }
+  if(playerSelection === computerSelection){
+    result = "its a draw"
+  }else if(playerSelection === "paper" && computerSelection === "rock"){
+    playerscore++;
+    result = "you win"
+  } else if(playerSelection === "rock" && computerSelection === "scissors"){
+    playerscore++;
+    result = "you win"
+}else if(playerSelection === "scissors" && computerSelection === "paper"){
+    playerscore++;
+    result = "you win"
+  }else if(playerSelection === "rock" && computerSelection === "paper"){
+    computerscore++
+    result = "you lose"
+} else if(playerSelection === "paper" && computerSelection === "scissors"){
+    computerscore++
+    result = "you lose"
+  } else if(playerSelection === "scissors" && computerSelection === "rock"){
+    computerscore++
+    result = "you lose"
 
+  }
+  resultText.innerHTML = `result: ${result}`
+} 
+
+
+
+
+buttons.forEach((button) => {
   button.addEventListener('click', () => {
+    const playerSelection = button.textContent;
+    const computerSelection = getComputerChoice();
+    playertext.textContent = `player: ${playerSelection}`;
+    computerText.textContent = `computer: ${computerSelection}`;
+    playround(playerSelection, computerSelection);
+    console.log("Player Score:", playerscore);
+    console.log("Computer Score:", computerscore);
+    checkwinner(playerscore,computerscore)
+    computercounter.innerHTML = `computer score: ${computerscore}`
+    playercounter.innerHTML =`player score: ${playerscore}`
 
-    button.textContent
-    playertext.textContent = `player ${button.textContent}`
-    computerText.textContent = `computer: ${computerSelection}`
-    resultText.textContent = `result: ${playRound}`
-    
     
   });
 });
 
 
+const checkwinner = (playerscore,computerscore) => {
 
-function playRound(playerSelection,computerSelection = getComputerChoice()) {
-
-  if (playerSelection === computerSelection) {
-  
-    roundResult = ('its a tie');
-    return roundResult
-    
-  }else if(playerSelection == "rock" && computerSelection == "paper") {
-  
-    roundResult = ('you lose this round')
-    return roundResult
-  
-  }else if(playerSelection == "paper" && computerSelection == "scissors") {
-  
-    roundResult = ('you lose this round')
-    return roundResult
-  
-  }else if (playerSelection == "scissors" && computerSelection == "rock"){
-  
-    roundResult = ('you lose this round')
-    return roundResult
-  }else if (playerSelection == "rock" && computerSelection == "scissors"){
-  
-    roundResult = ('you win this round')
-    return roundResult
-  
-  }else if (playerSelection == "paper" && computerSelection == "rock"){
-  
-    roundResult = ('you win this round')
-    return roundResult
-  
-  }else if(playerSelection == "scissors" && computerSelection == "paper"){
-  
-    roundResult = ('you win this round')
-    return roundResult
-  
+  if (playerscore === 5) {
+    resultText.textContent = "result: you have beaten computer :D" 
+    document.getElementById("ROCK").disabled = true;
+    document.getElementById("PAPER").disabled = true;
+    document.getElementById("SCISSORS").disabled = true;
+    playerscore = 0
+    computerscore = 0
   }
-  
-
-   
+   if (computerscore === 5){
+    resultText.textContent = "result: you have lost computer wins game"
+    document.getElementById("ROCK").disabled = true;
+    document.getElementById("PAPER").disabled = true;
+    document.getElementById("SCISSORS").disabled = true;
+    playerscore = 0
+    computerscore = 0
   }
 
-playRound();
+
+  }
+
+  
+
